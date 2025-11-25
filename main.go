d862f4df5d072c17880cbb70be68c013a06db0b7
Luka Hietala <luka.hietala08@gmail.com>
2025-11-25 19:56:28 +0200

    simple storage for clips and signal based menus

--- main.go
+++ main.go
@@ -1,24 +1,128 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
-	"log"
 )
 
+const clipFile = "koirankoppi.json"
 
-// gets the current clipboard item
+type ClipManager struct {
+	Clips []string `json:"clips"`
+}
+
+// loads all clips from json store (koirankoppi)
+func (cm *ClipManager) Load() error {
+	file, err := os.Open(clipFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			cm.Clips = []string{}
+			return nil
+		}
+		return err
+	}
+	defer file.Close()
+	return json.NewDecoder(file).Decode(cm)
+}
+
+func (cm *ClipManager) Save() error {
+	file, err := os.Create(clipFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return json.NewEncoder(file).Encode(cm)
+}
+
+func checkRequiredPrograms() {
+	// just wayland app for now, maybe X11 in the future
+	// maybe wofi, rofi, etc too? now just dmenu because its the most common
+	required := []string{"wl-paste", "wl-copy", "dmenu"}
+	for _, program := range required {
+		if _, err := exec.LookPath(program); err != nil {
+			fmt.Printf("%s not found : %v\n", program, err)
+			os.Exit(1)
+		}
+	}
+}
+
 func getClipboard() (string, error) {
 	cmd := exec.Command("wl-paste")
 	out, err := cmd.Output()
 	return strings.TrimSpace(string(out)), err
 }
 
+func setClipboard(text string) error {
+	cmd := exec.Command("wl-copy")
+	cmd.Stdin = strings.NewReader(text)
+	return cmd.Run()
+}
+
+func addClip() {
+	cm := &ClipManager{}
+	cm.Load()
+	clip, err := getClipboard()
+	if err != nil {
+		fmt.Println("error getting clipboard", err)
+		return
+	}
+	// prevent duplicates, if one already exist then move it first
+	for i, c := range cm.Clips {
+		if c == clip {
+			cm.Clips = append(cm.Clips[:i], cm.Clips[i+1:]...)
+			break
+		}
+	}
+	cm.Clips = append([]string{clip}, cm.Clips...)
+	// limit to 25, TODO: make optional
+	if len(cm.Clips) > 25 {
+		cm.Clips = cm.Clips[:25]
+	}
+	cm.Save()
+}
+
+func selectClip() {
+	cm := &ClipManager{}
+	cm.Load()
+	if len(cm.Clips) == 0 {
+		fmt.Println("no clips")
+		return
+	}
+	// dmenu seperates options by \n so prepare the clips
+	// -i match insensitively, -l list vertically with number of lines, -p message/prompt
+	input := strings.Join(cm.Clips, "\n")
+	cmd := exec.Command("dmenu", "-i", "-l", "10", "-p", "Select clip:")
+	// pass stdin to dmenu to intrepret
+	cmd.Stdin = strings.NewReader(input)
+	// dmenu returns selected item as stdout
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("dmenu error:", err)
+		return
+	}
+	selected := strings.TrimSpace(string(out))
+	setClipboard(selected)
+}
 
 func main() {
+	// wl-copy, wl-paste, dmenu
+	checkRequiredPrograms()
+	// prevent same stuff being spammed to cm, 
+	lastClip := ""
+	// check wl-paste every one second
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
+	sigChan := make(chan os.Signal, 1)
+	// when receving signal from usr1, open clipboard menu :D
+	// kill -USR1 $(pgrep hauva)
+	// TODO: change it to daemon (systemd?)
+	signal.Notify(sigChan, syscall.SIGUSR1)
 	for {
 		select {
 		case <-ticker.C:
@@ -26,7 +130,14 @@ func main() {
 			if err != nil {
 				continue
 			}
-			log.Println(clip)
+			if clip != lastClip && clip != "" {
+				lastClip = clip
+				addClip()
+			}
+		case <-sigChan:
+			fmt.Println("signal received, opening menu")
+			selectClip()
 		}
 	}
 }
+
